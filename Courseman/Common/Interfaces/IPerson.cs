1990db3085d5363c667037addd456c6e711989bf
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-02 03:05:23 +0100

    Add: Directory structure of Common
    Add: Person Interface with Name, Age and IdentityNumber props

---
+++ Courseman/Common/Interfaces/IPerson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Courseman.Common.Interfaces
+{
+	public interface IPerson
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public int IdentityNumber { get; set; }
+	}
+}
+
