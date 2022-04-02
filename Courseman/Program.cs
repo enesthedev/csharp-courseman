57681a5f0c3dcbdedcbdebef2d6e2fde0b179e5d
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-02 03:13:43 +0100

    Add: .NET 6 new console application template information comments

--- Courseman/Program.cs
+++ Courseman/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/**
+ * .NET 6
+ * 
+ * Bu proje .NET 6 kullanılarak oluşturulmuştur. Yeni .NET 6 şablon bilgileri için alttaki adresi inceleyin
+ * https://aka.ms/new-console-template
+ */
 Console.WriteLine("Hello, World!");
 
