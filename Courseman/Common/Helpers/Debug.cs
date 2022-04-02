327a8428bab3e85d99df41039cfde9d4de390aea
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-02 16:01:44 +0100

    Add: Static void WriteLine method with string header and Dictionary debugItems

--- Courseman/Common/Helpers/Debug.cs
+++ Courseman/Common/Helpers/Debug.cs
@@ -3,7 +3,15 @@ namespace Courseman.Common.Helpers
 {
 	public static class Debug
 	{
-		
+		public static void WriteLine(string header, Dictionary<String, String> debugItems)
+        {
+			Console.WriteLine("\n");
+			Console.WriteLine(header);
+
+			foreach (KeyValuePair<string, string> kvp in debugItems) {
+				Console.WriteLine("{0}:\t{1}", kvp.Key, kvp.Value);
+            }
+        }
 	}
 }
 
