06885925652fed082c717dd5cdf77c2f427f0621
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-02 14:34:37 +0100

    Add: Methods addStudent and removeStudent for List<Student>

--- Courseman/Common/Classes/Course.cs
+++ Courseman/Common/Classes/Course.cs
@@ -1,11 +1,53 @@
 ﻿using System;
+using System.Linq;
+using Courseman.Common.Interfaces;
+
 namespace Courseman.Common.Classes
 {
-	public class Course
-	{
-		public Course()
-		{
-		}
-	}
+    public class Course : ICourse
+    {
+        private string _name { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private Academician _academician { get; set; }
+        public Academician Academician
+        {
+            get { return _academician; }
+            set { _academician = value; }
+        }
+
+        private List<Student> _students { get; set; }
+        public List<Student> Students
+        {
+            get { return _students; }
+            set { _students = value; }
+        }
+
+        public Course(string Name)
+        {
+            this.Name = Name;
+            this.Students = new List<Student>();
+        }
+
+        public Course addStudent(Student student)
+        {
+            if (!Students.Contains(student)) {
+                Students.Append(student);
+            }
+            return this;
+        }
+
+        public Course removeStudent(Student student)
+        {
+            if (Students.Contains(student)) {
+                Students.Remove(student);
+            }
+            return this;
+        }
+    }
 }
 
