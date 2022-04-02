fbd3d7c50b0ef4b67d56560a4020363fce054037
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-02 03:08:29 +0100

    Refactor: Added space between diffrent which props have diffrent types

--- Courseman/Common/Interfaces/IPerson.cs
+++ Courseman/Common/Interfaces/IPerson.cs
@@ -4,6 +4,7 @@ namespace Courseman.Common.Interfaces
 	public interface IPerson
 	{
 		public string Name { get; set; }
+
 		public int Age { get; set; }
 		public int IdentityNumber { get; set; }
 	}
