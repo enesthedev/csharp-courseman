ccdbc343cce8fda1ff6bf84f4f583f41843d6e31
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-02 03:08:49 +0100

    Add: Student interface with Class, Courses and Grades props

---
+++ Courseman/Common/Interfaces/IStudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Courseman.Common.Interfaces
+{
+	public interface IStudent : IPerson
+	{
+		public string Class { get; set; }
+
+		public Array Courses { get; set; }
+		public Array Grades { get; set; }
+	}
+}
+
