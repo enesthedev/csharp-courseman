24f2f6b64c9cfc046df134295dd991aeae757369
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-03 01:12:57 +0100

    Add: Helper class Input with ReadOptions methdo
    This method usefully for when you want to read option dialog values without using while and try catch code blocks

---
+++ Courseman/Common/Helpers/Input.cs
@@ -0,0 +1,28 @@
+﻿namespace Courseman.Common.Helpers
+{
+	public class Input
+	{
+		public static int ReadOptions(List<dynamic> listOfClasses)
+		{
+			try {
+				int selectedOption = Convert.ToInt32(Console.ReadLine());
+				dynamic selectedClass = listOfClasses.ElementAt(selectedOption);
+
+				if (!(selectedClass != null))
+					throw new ArgumentOutOfRangeException();
+
+				Console.Clear();
+
+				return selectedOption;
+
+			} catch (ArgumentOutOfRangeException) {
+
+				Console.WriteLine("\n");
+				Console.WriteLine("Girdiğiniz değer mevcut değil.\nLütfen yeni bir değer girin:");
+
+				return ReadOptions(listOfClasses);
+			}
+		}
+	}
+}
+
