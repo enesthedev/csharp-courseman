fa38ac332314002195fa16bca9ec98129078dcf4
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-03 16:45:57 +0100

    Add: Application interface with dynamic list props Courses and Students
    Add: New static method Run for handle application start

---
+++ Courseman/IApplication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Courseman
+{
+	public interface IApplication
+	{
+		List<dynamic> Courses { get; set; }
+		List<dynamic> Students { get; set; }
+
+		public static void Run();
+	}
+}
+
