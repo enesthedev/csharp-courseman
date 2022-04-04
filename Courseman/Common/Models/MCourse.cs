b2fcf5b22eed87bd41f7ad6b8ddc8541322c2a6e
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-04 17:40:18 +0100

    Add: Models directory with model of Course MCourse
    Add: Fillable and FriendlyProperties to MCourse

---
+++ Courseman/Common/Models/MCourse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Courseman.Common.Models
+{
+	public class MCourse
+	{
+		public static string[] Fillable = {
+			"Name",
+			"MidtermRatio",
+			"FinalRatio"
+		};
+
+		public static Dictionary<string, string> FriendlyProperties = new Dictionary<string, string> {
+			{ "Name", "kursun ismi" },
+			{ "MidtermRatio", "vize notu oranları" },
+			{ "FinalRatio", "final notu oranları" }
+		};
+	}
+}
+
