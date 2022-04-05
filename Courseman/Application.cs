08206e028493964df593daa3e0d61b473ce786ee
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-05 17:35:17 +0100

    Add: Source comment about Enums

--- Courseman/Application.cs
+++ Courseman/Application.cs
@@ -64,61 +64,28 @@ namespace Courseman
             }
 
 			for (int i = 0; i < Courses.ToArray().Length; i++) {
-				Application.WriteLine("{0}: {1}", false, i, Courses.ElementAt(i).Name);
+				Application.WriteLine("{0}: {1} (Akademisyen: {2})", false, i, Courses.ElementAt(i).Name, Courses.ElementAt(i).Academician.Name);
 			}
 
 			int selectedCourseIndex = Input.ReadOptions(Courses, true);
 
 			if (selectedCourseIndex == -1) {
+				Course course = new Course();
 
-				Application.WriteLine("Kurs oluşturma sihirbazına hoşgeldiniz\nLütfen kurs adını giriniz:", true);
-				string? courseName = Console.ReadLine();
+				if (!Wizard.Mount(
+					course,
+					"Kurs oluşturma sihirbazına hoşgeldiniz",
+					"(Sihirbazdan çıkmak için -1 yazabilirsiniz"
+				)) return Run();
 
-				if (string.IsNullOrEmpty(courseName)) {
-					Application.WriteLine("Girilen kurs ismi boş olamaz. (Örnek isim: {0}", true, Courses.ElementAt(0).Name);
-					Thread.Sleep(1500);
+				Courses.Add(course);
 
-					return Run();
-				}
-
-				Application.WriteLine("{0} adlı kursun vizelerinin yüzde ne kadar etkili olacağını giriniz (Varsayılan değer {1}):", true, courseName, Course.MIDTERM_RATIO);
-				string? courseMidtermRatio = Console.ReadLine();
-
-				if (string.IsNullOrEmpty(courseMidtermRatio))
-					courseMidtermRatio = Course.MIDTERM_RATIO.ToString();
-
-				if (!Int32.TryParse(courseMidtermRatio, out int numberValue)) { // numberValue değerinin atamasını yaptım, aynı zamanda TryParse ile string'in sayı değerlerini kontrol ettim.
-					Application.WriteLine("{0} adlı kursun vizeler için girdiğiniz değer sadece sayı içermelidir", true, courseName);
-					Thread.Sleep(1500);
-
-					return Run();
-				}
-
-				Application.WriteLine("{0} adlı kursun final sınavının yüzde ne kadar etkili olacağını giriniz (Varsayılan değer {1}):", true, courseName, Course.FINAL_RATIO);
-				string? courseFinalRatio = Console.ReadLine();
-
-				if (string.IsNullOrEmpty(courseFinalRatio))
-					courseFinalRatio = Course.FINAL_RATIO.ToString();
-
-				if (!Int32.TryParse(courseFinalRatio, out numberValue)) { // int numberValue değeri zaten atandığı için direk kullandım.
-					Application.WriteLine("{0} adlı kursun finaller için girdiğiniz değer sadece sayı içermelidir", true, courseName);
-					Thread.Sleep(1500);
-
-					return Run();
-				}
-
-				Courses.Add(new Course(
-						courseName,
-						Convert.ToInt32(courseMidtermRatio),
-						Convert.ToInt32(courseFinalRatio)
-					)	
-				);
-
-				Application.WriteLine("{0} adlı kurs başarıyla eklendi", true, courseName);
+				Application.WriteLine("{0} adlı kurs başarıyla oluşturuldu", true, course.Name);
+				Thread.Sleep(1000);
 
 				return Run();
 			}
-
+		
 			Course selectedCourse = Courses.ElementAt(selectedCourseIndex);
 
 			Application.WriteLine("{0} kursu için lütfen alttaki öğrencilerden birini seçiniz:\nYeni bir öğrenci oluşturmak istiyorsanız -1 yazabilirsiniz.", true, selectedCourse.Name);
@@ -130,58 +97,24 @@ namespace Courseman
 			int selectedStudentIndex = Input.ReadOptions(Students, true);
 
 			if (selectedStudentIndex == -1) {
+				Student student = new Student();
 
-				Application.WriteLine("Öğrenci oluşturma sihirbazına hoşgeldiniz\nLütfen yeni öğrenci adını giriniz:", true);
-				string? studentName = Console.ReadLine();
+				if (!Wizard.Mount(
+					student,
+					"Öğrenci oluşturma sihirbazına hoşgeldiniz",
+					"(Sihirbazdan çıkmak için -1 yazabilirsiniz)"
+				)) return Run();
 
-				if (string.IsNullOrEmpty(studentName)) {
-					Application.WriteLine("Girilen öğrenci ismi boş olamaz. (Örnek isim: Enes Bayraktar)", true);
-					Thread.Sleep(1500);
+				Students.Add(student);
 
-					return Run();
-				}
-
-				Application.WriteLine("{0} adlı öğrencinin yaşını giriniz (Varsayılan değer {1}):", true, studentName, Student.STUDENT_AGE);
-				string? studentAge = Console.ReadLine();
-
-				if (string.IsNullOrEmpty(studentAge))
-					studentAge = Student.STUDENT_AGE.ToString();
-
-				if (!Int32.TryParse(studentAge, out int numberValue)) {
-					Application.WriteLine("{0} adlı öğrencinin yaşı için girdiğiniz değer sadece sayı içermelidir.", true, studentName);
-					Thread.Sleep(1500);
-
-					return Run();
-                }
-
-				Application.WriteLine("{0} adlı öğrencinin kimlik numarasını giriniz (Varsayılan değer {1}):", true, studentName, IDENTITY_NUMBER);
-				string? studentIdentityNumber = Console.ReadLine();
-
-				if (string.IsNullOrEmpty(studentIdentityNumber))
-					studentIdentityNumber = IDENTITY_NUMBER.ToString();
-
-				if (!Int64.TryParse(studentIdentityNumber, out long longValue)) {
-					Application.WriteLine("{0} adlı öğrencinin kimlik numarası sadece sayı içermelidir.", true, studentIdentityNumber);
-					Thread.Sleep(1500);
-
-					return Run();
-                }
-
-				Students.Add(new Student(studentName, Convert.ToInt32(studentAge), Convert.ToInt64(studentIdentityNumber)));
-
-				Application.WriteLine("{0} adlı öğrenci başarıyla eklendi", true, studentName);
-
-				selectedStudentIndex = Students.ToArray().Length - 1;
-
+				Application.WriteLine("{0} adlı öğrenci başarıyla eklendi", true, student.Name);
 				Thread.Sleep(1000);
-
-				Application.WriteLine("", true);
 
 				return Run();
 			}
 
 			Student selectedStudent = Students.ElementAt(selectedStudentIndex);
-
+			
 			return 1;
 		}
 
