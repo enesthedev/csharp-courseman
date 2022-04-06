db43ac081b5827c766c54e5ccb0479978ce32060
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-06 12:26:39 +0100

    Add: IsDouble and ReadDouble methods

--- Courseman/Common/Helpers/Input.cs
+++ Courseman/Common/Helpers/Input.cs
@@ -20,13 +20,47 @@
 				return selectedOption;
 
 			} catch (Exception ex) {
+
 				if (ex is ArgumentOutOfRangeException || ex is FormatException) {
-					Console.WriteLine("Girdiğiniz değer mevcut değil.\nLütfen yeni bir değer girin:");
+					Console.WriteLine(
+						"Girdiğiniz değer mevcut değil.\nLütfen yeni bir değer girin:"
+					);
 					return ReadOptions(listOfClasses, minusOneEnabled);
 				}
 				throw;
 			}
 		}
+
+		public static double ReadDouble(double min = Double.MinValue, double max = Double.MaxValue)
+        {
+			try {
+	
+				double value = Convert.ToDouble(
+					Console.ReadLine()
+				);
+
+				if (value > max || min > value)
+					throw new ArgumentOutOfRangeException();
+
+				return value;
+
+			} catch (Exception ex) {
+
+				if (ex is ArgumentOutOfRangeException || ex is FormatException) {
+					Application.WriteLine(
+						"Girdiğiniz değer mevcut değil.\nLütfen yeni bir değer girin:",
+						true
+					);
+					return ReadDouble(min, max);
+				}
+				throw;
+			}
+		}
+
+		public static bool IsDouble(string value)
+        {
+			return value.IndexOf(".") != -1;
+        }
 	}
 }
 
